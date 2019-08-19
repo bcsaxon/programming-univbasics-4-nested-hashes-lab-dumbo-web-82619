f1d268232151c4aa524356a8634033131678f5aa
Brett Saxon <brettcsaxon@gmail.com>
2019-08-19 15:48:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,9 +86,9 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {
+programmer_hash[:yukihiro_matsumoto] = "
 		:known_for => "Ruby",
-    :languages => ["LISP", "C"]}
+    :languages => ["LISP", "C"]"
 
 		puts programmer_hash
 end
